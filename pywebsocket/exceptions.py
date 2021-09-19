3abd5ba332f0401c3bc366fe32ff4baf81696aa9
Ege Bilecen <egebilecen@icloud.com>
2021-09-19 19:49:47 +0300

    added custom exceptions

---
+++ pywebsocket/exceptions.py
@@ -0,0 +1,34 @@
+"""
+    Author: Ege Bilecen
+"""
+
+## Raised when invalid method detected.
+class INVALID_METHOD(Exception):
+    pass
+
+## Raised when WebsocketServer.send_to_all receives an invalid send method as parameter.
+class INVALID_SEND_METHOD(Exception):
+    pass
+
+## Raised when length of data passed to WebsocketServer._encode_data is bigger than 0xFFFFFFFFFFFFFFFF.
+class DATA_LENGTH_ERROR(Exception):
+    pass
+
+## Raised when an unknown OPCODE detected.
+class UNKNOWN_OPCODE(Exception):
+    pass
+
+
+## Raised when closing OPCODE detected.
+class CLOSE_CONNECTION(Exception):
+    pass
+
+
+## Raised when unmasked frame detected.
+class MASK_ERROR(Exception):
+    pass
+
+
+## Raised when socket id is not in client socket list.
+class INVALID_SOCKET_ID(Exception):
+    pass
