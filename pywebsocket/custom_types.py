caeb7f41905c681253089e90b37553442ab65afe
Ege Bilecen <egebilecen@icloud.com>
2021-09-19 21:29:00 +0300

    added control frames

--- pywebsocket/custom_types.py
+++ pywebsocket/custom_types.py
@@ -7,3 +7,10 @@
 class FrameType:
     TEXT_FRAME   = 0x01
     BINARY_FRAME = 0x02
+
+## ControlFrame
+# Contains the constants that specifies the control frames.
+class ControlFrame:
+    CLOSE_FRAME = 0x08
+    PING_FRAME  = 0x09
+    PONG_FRAME  = 0x0A
